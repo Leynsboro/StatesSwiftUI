8c805c5d0128373d620023897ad3b7ca45670734
Ilya <leynsboro@gmail.com>
2022-09-09 15:19:47 +0300

    Initial Commit

---
+++ StatesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatesSwiftUI
+//
+//  Created by Илья Гусаров on 09.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StatesSwiftUI/StatesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatesSwiftUIApp.swift
+//  StatesSwiftUI
+//
+//  Created by Илья Гусаров on 09.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct StatesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
